0486d3600159463396e7f5fe5d2705ecb9696053
thegouge <thegouge42@gmail.com>
2021-03-01 13:12:27 -0800

    first chapter done

---
+++ src/components/Task.stories.js
@@ -0,0 +1,40 @@
+import { action } from '@storybook/addon-actions';
+import Task from './Task';
+
+export default { title: 'Task', component: Task, excludeStories: /.*Data$/ };
+
+export const actionsData = {
+	onPinTask: action('pin-task'),
+	onArchiveTask: action('archive-task'),
+};
+
+const Template = (args, { argTypes }) => ({
+	components: { Task },
+	props: Object.keys(argTypes),
+	methods: actionsData,
+	template: `<Task v-bind="$props" @pin-task="onPinTask" @archive-task="onArchiveTask" />`,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+	task: {
+		id: '1',
+		title: 'Test Task',
+		state: 'TASK_INBOX',
+		updatedAt: new Date(2021, 0, 1, 9, 0),
+	},
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+	task: { ...Default.args.task, title: 'herp derp', state: 'TASK_PINNED' },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+	task: {
+		...Default.args.task,
+		title: 'archived derp',
+		state: 'TASK_ARCHIVED',
+	},
+};
